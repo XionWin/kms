9ccb3f40cb783f69e4dedddcd36308ceeb695c32
XionWin <xionwin@hotmail.com>
2024-01-10 16:59:33 +0800

    Update

--- app/src/main.rs
+++ app/src/main.rs
@@ -35,7 +35,7 @@ fn main() {
 
     let (width, height) = (drm.crtc.get_width(), drm.crtc.get_height());
 
-    let gbm = gbm_rs::Gbm::new(
+    let mut gbm = gbm_rs::Gbm::new(
         fd,
         width,
         height,
@@ -69,4 +69,49 @@ fn main() {
         true,
     );
     print_warning!("context: {:#?}", context);
+
+    initialize(&context, &mut gbm, &drm);
 }
+
+pub fn initialize(context: &egl_rs::Context, gbm: &mut gbm_rs::Gbm, drm: &drm_rs::Drm) {
+    let surface = gbm.get_surface_mut();
+
+    let display_handle = context.display;
+    let surface_handle = context.surface;
+
+    let func = |display: *const libc::c_void, surface: *const libc::c_void| unsafe {
+        egl_rs::swap_buffers(display, surface)
+    };
+    surface.register_swap_callback((func, display_handle as _, surface_handle as _));
+
+    let (_, fb) = surface.lock();
+    let drm_fd = drm.get_fd();
+    let drm_crtc_id = drm.crtc.get_id();
+    let drm_connector_ids = &vec![drm.connector.get_id()];
+    let drm_mode = drm.get_mode().get_handle();
+    match drm_rs::set_crtc(
+        drm_fd,
+        drm_crtc_id,
+        fb as _,
+        0,
+        0,
+        drm_connector_ids.as_ptr(),
+        drm_connector_ids.len() as _,
+        drm_mode,
+    ) {
+        result if result == 0 => result,
+        _ => panic!("surface initialize set_crtc error"),
+    };
+}
+
+pub fn update(context: &mut egl_rs::Context, gbm: &mut gbm_rs::Gbm, drm: &drm_rs::Drm) {
+    let fd = drm.get_fd();
+    let crtc_id = drm.crtc.get_id();
+
+    let surface = gbm.get_surface_mut();
+    let (_, fb) = surface.lock();
+    if context.vertical_synchronization {
+        drm_rs::vertical_synchronization(fd, crtc_id, fb);
+    }
+}
+
