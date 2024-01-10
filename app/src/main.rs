e0d0483f280b89ed6a4bb0073f4770eb29d4b091
XionWin <xionwin@hotmail.com>
2024-01-11 01:21:57 +0800

    Update

--- app/src/main.rs
+++ app/src/main.rs
@@ -78,11 +78,12 @@ fn main() {
     
     gles_rs::viewport(0, 0, width, height);
     loop {
-        gles_rs::clear_color(1f32, 1f32, 0f32, 0.3f32);
-        // let angle = 0; //(std::time::SystemTime::now().duration_since(*first_tick).unwrap().as_millis() / 20 % 360) as u32;
-        // let hsv = drawing::color::HSV::new(angle as f32, 1.0f32, 0.2f32);
-        // let rgb: drawing::color::RGB = hsv.into();
-        // let (r, g, b) = rgb.into();
+        // gles_rs::clear_color(1f32, 1f32, 0f32, 0.3f32);
+        let angle = 0; //(std::time::SystemTime::now().duration_since(*first_tick).unwrap().as_millis() / 20 % 360) as u32;
+        let hsv = color_rs::HSV::new(angle as f32, 1.0f32, 0.2f32);
+        let rgb: color_rs::RGB = hsv.into();
+        let (r, g, b) = rgb.into();
+        gles_rs::clear_color(r as f32 / 255f32, g as f32 / 255f32, b as f32 / 255f32, 0.3f32);
 
         gles_rs::clear(0x00004000);
         context.frame_vertical_synchronize(&mut gbm, &drm);
