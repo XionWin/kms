414533a929c7da36c3f40872010d0189dbc05f5a
XionWin <xionwin@hotmail.com>
2024-01-13 02:07:34 +0800

    Update

--- app/src/main.rs
+++ app/src/main.rs
@@ -15,7 +15,7 @@ fn main() {
         utility::pretty_print_system_time(SystemTime::now()).green()
     );
 
-    kms_rs::init(Some("/dev/dri/card0"), init_func, update_fun);
+    kms_rs::init(Some("/dev/dri/card0"), kms_rs::SurfaceType::OpenGlesV2, init_func, update_fun);
 }
 
 fn init_func(context: &kms_rs::Context) {
